57204e17e14eb02bf5ebfd086fc9782030cdd7e2
shoujianzhang <zhangshoujian@hotmail.com>
2019-06-26 08:03:49 +0900

    version 0.0.1

--- App.js
+++ App.js
@@ -1,49 +1,167 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Alert, TouchableOpacity, Image, StyleSheet, View, Linking, Platform } from 'react-native';
+import {
+  Icon
+} from 'native-base';
+import { RNCamera } from 'react-native-camera';
+import BarcodeMask from 'react-native-barcode-mask';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class BarcodeScan extends Component {
+  constructor(props) {
+    super(props);
+    this._handleTourch = this._handleTourch.bind(this);
+    this._handleCameraType = this._handleCameraType.bind(this);
+    this.state = {
+      torchOn: false,
+      backCameraOn: true,
+    }
+    this.isBarcodeRead = false;
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  _requestPermissions = async () => {
+    if (Platform.OS === 'android') {
+      const result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA)
+      return result === PermissionsAndroid.RESULTS.GRANTED || result === true
+    }
+    return true
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  _onBarCodeRead = (e) => {
+    if (!this.isBarcodeRead) {
+      this.isBarcodeRead = true;
+      console.log("Barcode value is" + e.data, "Barcode type is" + e.type);
+      Alert.alert(
+        null,
+        e.data,
+        [
+          {text: 'Cancel', onPress: () => {this.isBarcodeRead = false}},
+          {text: 'Open with brower', onPress: () => this._openReadByBrower(e)},
+        ],
+        { cancelable: false }
+      )
+    }
+  }
+
+  // open read data by brower.
+  _openReadByBrower(e) {
+    Linking.canOpenURL(e.data)
+      .then((supported) => {
+        if (!supported) {
+          return Linking.openURL('https://www.google.co.jp/search?q=' + e.data);
+        } else {
+          return Linking.openURL(e.data);
+        }
+      })
+      .catch((err) => console.error('An error occurred', err));
+    this.isBarcodeRead = false;
+  }
+
+  // toggle flash light.
+  _handleTourch(value) {
+    if (value === true) {
+      this.setState({ torchOn: false });
+    } else {
+      this.setState({ torchOn: true });
+    }
+  }
+  
+  // toggle camera between front and back.
+  _handleCameraType(value) {
+    if (value === true) {
+      this.setState({ backCameraOn: false });
+    } else {
+      this.setState({ backCameraOn: true });
+    }
+  }
+
+  componentDidMount = () => {
+    ({ _, status }) => {
+      if (status !== 'PERMISSION_GRANTED') {
+        this._requestPermissions()
+      }
+    }
+  }
+   
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <RNCamera
+          style={styles.preview}
+          ref={cam => this.camera = cam}
+          flashMode={this.state.torchOn ? RNCamera.Constants.FlashMode.torch : RNCamera.Constants.FlashMode.off}
+          type={this.state.backCameraOn ? RNCamera.Constants.Type.back : RNCamera.Constants.Type.front}
+          captureAudio={false}
+          barCodeTypes={[RNCamera.Constants.BarCodeType.ean8, RNCamera.Constants.BarCodeType.ean13, RNCamera.Constants.BarCodeType.upce]}
+          onBarCodeRead={this._onBarCodeRead}
+        >
+          <BarcodeMask edgeColor={'green'} showAnimatedLine={false}/>
+        </RNCamera>
+        <View style={styles.bottomOverlayLeft}>
+          <TouchableOpacity onPress={() => this._handleTourch(this.state.torchOn)}>
+            <View style={[styles.iconContainer, this.state.torchOn === true ? styles.lightOn : null]}>
+              <Icon type={Platform.OS === 'ios' ? 'AntDesign' : 'MaterialCommunityIcons'} name={Platform.OS === 'ios' ? 'eye' : 'flashlight' } style={ styles.icon }/>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this._handleCameraType(this.state.backCameraOn)}>
+            <View style={styles.iconContainer}>
+              <Icon type={Platform.OS === 'ios' ? 'AntDesign' : 'MaterialCommunityIcons'} name={Platform.OS === 'ios' ? 'camera' : 'camera-switch'} style={styles.icon}/>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.bottomOverlayRight, {height: 0, opacity: 0}]}>
+          <TouchableOpacity onPress={() => this._handleCameraType(this.state.backCameraOn)}>
+              <Icon type="AntDesign" name={ 'closecircle'} style={styles.closeIcon}/>
+          </TouchableOpacity>
+        </View>
       </View>
-    );
+    )
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  bottomOverlayLeft: {
+    position: "absolute",
+    width: "100%",
+    flex: 20,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  bottomOverlayRight: {
+    position: "absolute",
+    width: "100%",
+    flex: 20,
+    flexDirection: "row",
+    justifyContent: "flex-end"
+  },
+  icon: {
+    color: 'black',
+  },
+  closeIcon: {
+    color: '#007AFF',
+    marginTop: 10,
+    marginRight: 10,
+    fontSize: 40,
+  },
+  iconContainer: {
+    marginTop: 10,
+    marginLeft: 10,
+    backgroundColor: '#808080',
+    opacity: 1.00,
+    borderRadius: 23,
+    paddingHorizontal: 8,
+    paddingTop: 9,
+    paddingBottom: 7,
+  },
+  lightOn: {
+    backgroundColor: 'white',
+    opacity: 1.00,
   },
 });
